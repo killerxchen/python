ae83b946682bf12936724dd71f447fadf429348d
killerxchen <41565590+killerxchen@users.noreply.github.com>
2018-08-10 18:11:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-    flag = "resblockName"
-    xiaoqu = soup.find(text=re.compile(flag))
-    xiaoquStartIndex = xiaoqu.find(flag) + len(flag)
+    communityNameflag = "resblockName"
+    communityNamediv = soup.find(text=re.compile(communityNameflag))
+    communityNameStartIndex = communityNamediv.find(communityNameflag) + len(communityNameflag)
+    communityName = communityNamediv[communityNameStartIndex + 2 :]
+    communityNameEndIndex = communityName.find(",")
+    print(communityName[0:communityNameEndIndex - 1])
 
-    xiaoqu = xiaoqu[xiaoquStartIndex+ 2 :]
-    xiaoquEndIndex = xiaoqu.find(",")
-    print(xiaoqu[0:xiaoquEndIndex - 1])
+    communityIdflag = "resblockId"
+    communityIddiv = soup.find(text=re.compile(communityIdflag))
+    communityIdStartIndex = communityIddiv.find(communityIdflag) + len(communityIdflag)
+    communityId = communityIddiv[communityIdStartIndex + 2 :]
+    communityIdEndIndex = communityId.find(",")
+    print(communityId[0:communityIdEndIndex - 1])
